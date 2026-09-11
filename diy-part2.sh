e7c1d57e7a09fec259b011e7bec36993a52e9b08
dc021026 <16941698@qq.com>
2026-09-11 22:10:16 +0800

    Update default IP and enable WiFi
    Change default IP address and enable WiFi.

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+# 修改默认 IP 地址为 192.168.2.1
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+
 # 启用 WiFi
 sed -i 's/disabled=1/disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh || true
