42da90568d3deacd503ebdd9a75aeacd9791cb80
dc021026 <16941698@qq.com>
2026-09-11 22:35:30 +0800

    Update helloworld feed in feeds.conf.default
    Remove potential duplicate helloworld feed and add it back.

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+# 移除可能存在的重复 helloworld feed
+sed -i "/helloworld/d" "feeds.conf.default"
+
 # 添加 helloworld feed
-echo 'src-git helloworld https://github.com/fw876/helloworld.git' >>feeds.conf.default
+echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
